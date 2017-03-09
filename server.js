a97221ca73edf9c5d4eef5e7d3fe5cbf94157820
jbentley10 <jwbentle@gmail.com>
2017-03-09 08:18:29 -0800

    Added block of code to avoid  error

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ var expressLayouts = require('express-ejs-layouts');
 var app = express(); 
 var port = 5000;
 
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 // use ejs and express layouts
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
@@ -17,6 +27,6 @@ app.use(express.static(__dirname + '/public'));
 
 // Start the server
 app.listen(port, function() {
-  console.log('app started');
+console.log('app started');
 });
 
